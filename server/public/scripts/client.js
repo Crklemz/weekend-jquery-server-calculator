7c53a8457fc5a7697566a42b8a74e21bfa9076b1
Chris Klemz <crklemz@gmail.com>
2021-05-21 17:18:17 -0500

    buttons created and working on client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,44 @@ $(handleReady);
 function handleReady() {
     console.log('JQ');
 
+    //click listeners
+    $('#addBtn').on('click', handleAdd);
+    $('#subBtn').on('click', handleSubtract);
+    $('#multBtn').on('click', handleMultiply);
+    $('#divBtn').on('click', handleDivide);
+    $('#calcBtn').on('click', handleCalulate);
+    $('#clrBtn').on('click', handleClear);
 
+    //functions to call on ready
+    
+}
+
+function handleAdd() {
+    console.log('clicked addBtn');
+    
+}
+
+function handleSubtract() {
+    console.log('clicked subBtn');
+    
+}
+
+function handleMultiply() {
+    console.log('clicked multBtn');
+    
+}
+
+function handleDivide() {
+    console.log('clicked divBtn');
+    
+}
+
+function handleCalulate() {
+    console.log('clicked calcBtn');
+    
+}
+
+function handleClear() {
+    console.log('clicked clrBtn');
     
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-
+let 
 
 app.use(express.static('server/public')); 
 app.use(bodyParser.urlencoded({extended : true}));
